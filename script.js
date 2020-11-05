a7dac0875896b6b011119aff2544d43ca9a76c6e
beingstoic <59771568+beingstoic@users.noreply.github.com>
2020-11-05 07:53:58 +0530

    added functionality of restart and changed the way it stores elements in an array

--- script.js
+++ script.js
@@ -9,8 +9,58 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+let card = document.querySelectorAll('.card');
+let ctr=0;
+let arrayCtr=-1;
+let matchedCtr = 0;
+let isMatched = false;
+let cardStore = [];
+let restart = document.querySelector('.restart')
+// This function will be responsible to listen to every clicks and then send the card to another function for card matching.
+card.forEach((cardElement)=>{
+    cardElement.addEventListener('click',(event)=>{
+        ctr =ctr+1;
+            if(ctr!==0 && ctr<=2) 
+            {   cardElement.classList.add('show', 'open');
+            arrayCtr++;
+            cardStore[arrayCtr]=cardElement;
+            console.log(cardStore)
+            if(ctr===2){
+                console.log(arrayCtr-1);
+                isMatched = getMatch(cardStore,arrayCtr);
+                console.log(arrayCtr-1);
+                    ctr=0;
+                    if(matchedCtr)  matchedCtr++;
+            }
+        } 
+        if(matchedCtr===8){
+            console.log('seems like we can create a modal')
+        }   
+    }
+)})
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+
+// This function takes the card elements which are there to be compared and then changing the classes according to the matches
+const getMatch = (cardStore, arrayCtr)=>{
+    let firstCard = cardStore[arrayCtr-1].firstElementChild.className;
+    let secondCard=cardStore[arrayCtr].firstElementChild.className;
+    if(firstCard===secondCard){
+        
+        cardStore[arrayCtr].classList.add('match');
+        cardStore[arrayCtr-1].classList.add('match');
+        return true;
+    }
+    setTimeout(()=>{
+        cardStore[arrayCtr].classList.remove('show','open');
+        cardStore[arrayCtr-1].classList.remove('show','open');
+    }, 500)
+    
+    return false;
+}
+
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976 which is right now not implemented in the code.
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -38,6 +88,9 @@ function shuffle(array) {
  */
 
 
- addEventListener('click',()=>{
-     
+ restart.addEventListener('click',()=>{
+     cardStore.forEach((cardElement)=>{
+         cardElement.classList.remove('match', 'show','open');
+     })
+
  })
